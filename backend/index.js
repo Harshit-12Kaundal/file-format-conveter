6ce1d5dcb2c26664df484cc91da3a23071aee07b
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2024-05-18 15:55:05 +0530

    adding some backend  codes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express =require('express');
+const multer =require('multer');
+const docxTopdf= require('docx-pdf');
+const path = require('path');
+
+
 const app = express();
 const port =3001
-const multer =require('multer');
-
-
 // setting up the file storage
 
 const storage =multer.diskStorage({
@@ -18,11 +20,32 @@ const storage =multer.diskStorage({
 const upload = multer({ dest: 'uploads/' })
 
 
-app.post('/profile', upload.single('file'), function (req, res, next) {
+app.post('/convertfile', upload.single('file'), (req, res, next)=> {
     try {
+        if(!req.file){
+            return res.status(400).json({
+                message:"No file was uploaded"
+            });
+        }
+        let outputpath = path.join(__dirname, 'files',`${req.file.originalname}.pdf`)
+            docxTopdf(req.file.path,outputpath,(err,result)=>{
+            if(err){
+            console.log(err);
+            return res.status(500).json({
+                message:"error in converting Docx to Pdf file"
+            })
+            }
+            console.log('result'+result);
+            res.download(outputpath ,() =>{
+                console.log("file Downloaded")
+            })
+        });
         
     } catch (error) {
         console.log(error);
+        res.status(500).json({
+            message:"Internal server error"
+        })
     }
 })
   
