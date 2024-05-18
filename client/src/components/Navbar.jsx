a19ef83aa7820c38c51627917bec262d31ca6e79
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2024-05-18 16:07:32 +0530

    adding the frontend

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='max-w-screen-2xl mx-anto container px-6 md:px-40 '>
+            <div className='flex justify-between'>
+                <h1>Word To Pdf</h1>
+                <h1>Home</h1>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
