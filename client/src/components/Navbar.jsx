10ce95aac9389dd35125f757974f934a3c357139
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2024-06-01 09:26:31 +0530

    changing the name

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
     <div className="max-w-screen-full mx-auto px-6 py-3 md:px-40 shadow-lg h-16 flex items-center justify-between">
-      <h1 className="text-2xl cursor-pointer font-bold">Word<span className="text-4xl text-green-500">To</span>Pdf</h1>
+      <h1 className="text-2xl cursor-pointer font-bold">File<span className="text-4xl text-green-500">Format</span>Converter</h1>
       <h1 className="text-2xl cursor-pointer font-bold hover:scale-125 duration-300">Home</h1>
     </div>
     </>
