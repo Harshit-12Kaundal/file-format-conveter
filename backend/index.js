544fe094c40ff77688a197387dfc941b79d3d43d
Harshit-12Kaundal <97866054+Harshit-12Kaundal@users.noreply.github.com>
2024-10-14 14:48:32 +0530

    adding the production url in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,34 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const cors =require('cors')
+const cors = require('cors');
 require('dotenv').config(); 
 
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
+
+// Determine the environment and set the CORS origin
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' ? process.env.CORS_ORIGIN_PROD : process.env.CORS_ORIGIN_DEV,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 const upload = multer({ dest: 'uploads/' });
 
 const docxRoutes = require('./routes/docxRoute');
 const pdfRoutes = require('./routes/pdfRoute');
-const { config, configDotenv } = require('dotenv');
 
-app.get('/', (req, res)=>{
-    res.status(200).json({'message':'hello'});
-})
+// Basic route to test the server
+app.get('/', (req, res) => {
+  res.status(200).json({ 'message': 'hello' });
+});
 
 app.use('/convertfile/docx', docxRoutes);
 app.use('/convertfile/pdf', pdfRoutes);
 
+// Set the port from .env or default to 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
